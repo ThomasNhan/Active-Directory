533b755d707efe6ed1587b69f05e5f187729f8d8
ThomasNhan <49893948+ThomasNhan@users.noreply.github.com>
2019-04-28 03:11:56 -0700

    Add files via upload

---
+++ Assignment10/Script 10.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module ActiveDirectory
+$csvpath= 'C:\Users\Administrator\Desktop\CSV\users.csv'
+$csvfile = Import-Csv -path $csvpath
+foreach ($user in $csvfile){
+    $username = $user.username
+    $firstname = $user.firstname
+    $lastname = $user.lastname
+    $description = $user.description
+    $OUPATH = $user.oupath
+    $Action = $user.action
+    if ($Action -like "Add")
+    {New-ADUser -name $username -GivenName $firstname -Surname $lastname -Description $description -Path $OUPATH}
+
+    elseif ($Action -like "Remove")
+    {Remove-ADUser -Identity $username}
+    }
